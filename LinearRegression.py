e2021e27cf5b6a20a74af22bde7e188dd70136d3
Nikodem <94792326+AuRocket@users.noreply.github.com>
2023-09-26 18:24:08 +0200

    Update and rename testowa.py to LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,33 +1,33 @@
 import numpy as np
 
 class LinearRegression:
-    def __init__(self, w, b):
-        self.w = w
-        self.b = b
+    def __init__(self):
+        self.w = None
+        self.b = None
+        self.m = None
+        self.n = None
 
     def predict(self, x):
         return np.dot(self.w, x) + self.b
 
     def compute_cost(self, X, y):
-        m, n = X.shape
         err = 0
-        for i in range(m):
+        for i in range(self.m):
             err += np.square(self.predict(X[i]) - y[i])
-        err = err / (2*m)
+        err = err / (2*self.m)
 
         return err
 
     def compute_gradient(self, X, y):
-        m, n = X.shape
         dj_db = 0
-        dj_dw = np.zeros(n)
+        dj_dw = np.zeros(self.n)
 
-        for i in range(m):
+        for i in range(self.m):
             dj_db += self.predict(X[i]) - y[i]
-            for j in range(n):
+            for j in range(self.n):
                 dj_dw[j] += (self.predict(X[i]) - y[i]) * X[i][j]
-        dj_db = dj_db / m
-        dj_dw = dj_dw / m
+        dj_db = dj_db / self.m
+        dj_dw = dj_dw / self.m
 
         return dj_dw, dj_db
 
@@ -40,13 +40,13 @@ class LinearRegression:
             self.w = w_tmp
             self.b = b_tmp
 
-            if i % 10 == 0:
+            if i % (iters//10) == 0:
                 print(f'iteration: {i} cost: {self.compute_cost(X, y)}')
 
         return self.w, self.b
 
     def fit(self, X, y, alpha=0.01, iters=10):
-        m, n = X.shape
-        self.w = np.random.rand(n)
+        self.m, self.n = X.shape
+        self.w = np.random.rand(self.n)
         self.b = 100
         self.w, self.b = self.gradient_descent(X, y, alpha, iters)
